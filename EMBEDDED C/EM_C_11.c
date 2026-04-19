b118466f5e95cce5ec65732397bb55bc54d72929
Bharath <chandranbharath02@gmail.com>
2026-04-19 20:36:24 +0530

    Add files via upload

---
+++ EMBEDDED C/EM_C_11.c
@@ -0,0 +1,54 @@
+#include <reg52.h>
+sbit RS = P3^0;
+sbit RW = P3^1;
+sbit EN = P3^2;
+
+#define LCD P2
+
+void delay(){
+	int i,j;
+	for(i=0;i<200;i++){
+		for(j=0;j<200;j++){
+		}
+	}
+}
+void cmd (unsigned char c){
+	RS = 0;
+	RW = 0;
+	LCD = c;
+	EN = 1;
+	delay();
+	EN = 0;
+}
+void dat (unsigned char d){
+	RS = 1;
+	RW = 0;
+	LCD = d;
+	EN = 1;
+	delay();
+	EN = 0;
+} 
+void main(){
+	cmd(0x38);
+	cmd(0x0c);
+	cmd(0x06);
+	cmd(0x01);
+	
+	dat('8');
+	dat('0');
+	dat('5');
+	dat('1');
+	
+	cmd(0xC0);
+	
+	dat('E');
+	dat('M');
+	dat('B');
+	dat('E');
+	dat('D');
+	dat('D');
+	dat('E');
+	dat('D');
+	
+	while(1);
+}
